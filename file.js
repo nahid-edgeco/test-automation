aa01a042aabaf2445ff4c1940303c8a82e2610cd
nahid-edgeco <nsultan@americantrust.com>
2023-02-08 12:55:26 +0600

    messagg

--- file.js
+++ file.js
@@ -5,10 +5,11 @@ console.log(b);
 const c = b + 1;
 const d = c + 8;
 const e = d + d;
-const monjirul = "jobless";
-const f = "Bangladesh";
-const message = "Can you see it?";
+const monjirul = 'jobless';
+const f = 'Bangladesh';
+const message = 'Can you see it?';
 const g = 0;
-const h = "hi";
-const message2 = "hello?";
-this is an error hi hi
+const h = 'hi';
+const message2 = 'hello?';
+// this is an error hi hi
+const message3 = 'Monjirul, dont keep wrong code!!';
