6ced1e684df5b61626fa44aa8d79648aa37f100f
nahid-edgeco <nsultan@americantrust.com>
2023-02-13 12:07:24 +0600

    hello world

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const getGoogle = async () => {
   const res = await axios.get('https://google.com');
   console.log({ res });
+  console.log('hello world!');
 };
 
 getGoogle();
